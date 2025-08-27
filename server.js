6409e2956fe93c201b182fe51a67d2fc9cd65422
ShreyaKhasne <shreyamukul8@gmail.com>
2025-08-27 17:38:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+// Import Express
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send('Welcome to Simple Express API!');
+});
+
+// GET API Route
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
+
+// POST API Route
+app.post('/api/data', (req, res) => {
+    const { name } = req.body;
+    res.json({ success: true, message: `Hello, ${name}!` });
+});
+
+// Start Server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
